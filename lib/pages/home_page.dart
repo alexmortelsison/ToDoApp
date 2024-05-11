1eb896fdc3e36e6e788f17fd2d3893ad43fa2112
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-05-11 21:54:53 +0900

    updated home_page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,6 +9,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List toDoList = [
+    ['Write some code', false],
+    ['Do Walking', false]
+  ];
+
+  void checkboxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,19 +28,13 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.transparent,
         title: const Text('ToDo'),
       ),
-      body: ListView(
-        children: [
-          ToDoTile(
-            taskName: 'Write some code',
-            taskCompleted: true,
-            onChanged: (p0) {},
-          ),
-          ToDoTile(
-            taskName: 'Do Exercise',
-            taskCompleted: false,
-            onChanged: (p0) {},
-          ),
-        ],
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) => ToDoTile(
+          taskName: toDoList[index][0],
+          taskCompleted: toDoList[index][1],
+          onChanged: (value) => checkboxChanged(value, index),
+        ),
       ),
     );
   }
