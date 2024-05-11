253ad8484754bd6d37852ff20e504661f2bb9e68
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-05-11 22:18:04 +0900

    updated dialog_box

--- lib/util/dialog_box.dart
+++ lib/util/dialog_box.dart
@@ -1,25 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:todo_app/util/my_button.dart';
 
 class DialogBox extends StatelessWidget {
   const DialogBox({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const AlertDialog(
+    return AlertDialog(
       backgroundColor: Colors.lightBlue,
       content: SizedBox(
         height: 120,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            TextField(
+            const TextField(
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
                 hintText: 'Add a New Task',
               ),
             ),
             Row(
-              children: [],
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                MyButton(text: 'Save', onPressed: () {}),
+                const SizedBox(width: 8),
+                MyButton(text: 'Cancel', onPressed: () {}),
+              ],
             ),
           ],
         ),
